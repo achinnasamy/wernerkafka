8adb04d5eefeb67474a9a6367575f2b3d352f742
achinnasamy <chinnasamyad@gmail.com>
2018-04-28 12:32:12 +0530

    More code

---
+++ src/com/dmac/producer/UIDConsumer.scala
@@ -0,0 +1,59 @@
+package com.dmac.producer
+
+import java.util.{Arrays, Properties}
+
+import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer}
+import org.apache.kafka.common.TopicPartition
+
+/**
+  * Created by dharshekthvel on 4/26/18.
+  */
+object UIDConsumer {
+
+  def main(args : Array[String]) = {
+
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    // When changed the consumer group, it behaves as a TOPIC or QUEUE
+    props.put("group.id", "AWS-CONSUMER5")
+    props.put("enable.auto.commit", "true")
+    //props.put("auto.commit.interval.ms", "1000")
+    props.put("session.timeout.ms", "30000")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+    /**
+      *
+      */
+    val consumer = new KafkaConsumer[String, String](props)
+
+    // Consumer consuming from a Particular Topic
+    //consumer.subscribe(Arrays.asList("ZETA-ENGINE-TOPIC"))
+
+    val partitionTwentyThree = new TopicPartition("KANDY-TOPIC", 23)
+    val partitionTwentyFour = new TopicPartition("KANDY-TOPIC", 24)
+    val partitionTwentyFive = new TopicPartition("KANDY-TOPIC", 25)
+    consumer.assign(Arrays.asList(partitionTwentyThree,partitionTwentyFour,partitionTwentyFive))
+
+    while (true) {
+
+      val records = consumer.poll(1)
+
+      records.forEach(new ForEachPrintOutputReckoner)
+
+      consumer.commitSync()
+      //consumer.commitAsync()
+
+    }
+
+
+  }
+
+
+}
+
+class ForEachPrintOutputReckoner extends java.util.function.Consumer[ConsumerRecord[String,String]] {
+  override def accept(t: ConsumerRecord[String,String]): Unit = {
+    println(t.key() + " - - - " + t.value())
+  }
+}
--- src/com/dmac/producer/UIDProducer.scala
+++ src/com/dmac/producer/UIDProducer.scala
@@ -9,33 +9,30 @@ import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecor
   */
 object UIDProducer {
 
-
   def main(args: Array[String]) = {
-
 
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092")
-    props.put("client.id", "AWS-ID")
+    props.put("client.id", "ZETA-PRODUCER-001")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("linger.ms", "1")
     props.put("batch.size", "445")
-
+    props.put("compression.codec", "1")
 
     val producer = new KafkaProducer[String, String](props)
 
-    val data = new ProducerRecord[String, String]("ANZ", 99, "key_12", "Ingesting data")
-    val data1 = new ProducerRecord[String, String]("ANZ", "Ingesting data")
-    val data2 = new ProducerRecord[String, String]("ANZ", "Ingesting data")
+    for (i <- 1 to 10) {
+      val data = new ProducerRecord[String, String]("KANDY-TOPIC", 23, "key_101", "Ingesting data on to OPEN_TSDB_1")
+      //val data2 = new ProducerRecord[String, String]("KANDY-TOPIC", 26, "key_102", "Ingesting data on to Voldemort_2")
+      //val data3 = new ProducerRecord[String, String]("KANDY-TOPIC", 79, "key_103", "Ingesting data on to StarDog_3")
 
+      producer.send(data)
+      producer.flush()
+      //producer.send(data2, oncallback)
+      //producer.send(data3)
+    }
 
-    producer.send(data, oncallback)
-
-    producer.send(data1, oncallback)
-    producer.send(data2, oncallback)
-
-
-    producer.flush()
 
   }
 
